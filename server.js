e610bb3aa810ed7764c367b2c63706e01d32f6f8
sukey <sukey.ong1@gmail.com>
2020-10-19 18:07:17 +0800

    try again

--- server.js
+++ server.js
@@ -47,7 +47,13 @@ app.use(passport.session());
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: "http://localhost:3000", // allow to server to accept request from different origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true // allow session cookie from browser to pass through
+  }
+));
 
 app.use('/notes', notesRouter);
 app.use('/users', usersRouter);
